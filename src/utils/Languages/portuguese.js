41578a4a67f9c8b6933cc5de74c50422dc62a72d
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2020-06-29 13:47:36 -0400

    fixed languages

--- src/utils/Languages/portuguese.js
+++ src/utils/Languages/portuguese.js
@@ -1,5 +1,107 @@
 const React = require("react");
 
+module.exports.naviBar = "ESSENCIA";
+
+module.exports.membersBar = "Mestre";
+
+module.exports.returnMenu = "Voltar";
+
+module.exports.progressBar = [
+  { name: "Raça", previous: "Cancelar", next: "Seguinte" },
+  { name: "Origem", previous: "Anterior", next: "Seguinte" },
+  { name: "Classe", previous: "Anterior", next: "Seguinte" },
+  { name: "Disciplinas", previous: "Anterior", next: "Seguinte" },
+  { name: "Defensivo", previous: "Anterior", next: "Seguinte" },
+  { name: "Ofensivo", previous: "Anterior", next: "Seguinte" },
+  { name: "Passado", previous: "Anterior", next: "Seguinte" },
+  { name: "Características", previous: "Anterior", next: "Seguinte" },
+  { name: "Tendência", previous: "Anterior", next: "Seguinte" },
+  { name: "Atributos", previous: "Anterior", next: "Seguinte" },
+  { name: "Perfil", previous: "Anterior", next: "Seguinte" },
+];
+
+module.exports.login = {
+  title: "ESSENCIA",
+  version: "Versão 0.0 - Alfa Fechado",
+  button: {
+    signIn: "Entrar",
+    signUp: "Cadastrar",
+  },
+  textBox: [
+    "Bem Vindo à ESSENCIA!",
+    "Um sistema de RPG moderno, simples e tático",
+    '"Domine o combate, use as habilidades e crie suas próprias magias. O limite é a suacriatividade nesse jogo de interpretação"',
+  ],
+  label: {
+    nickName: "Usuário",
+    email: "E-mail",
+    password: "Senha",
+  },
+  notFound: "Usuário não encontrado.",
+  incorrect: "Senha incorreta.",
+};
+
+module.exports.dashboard = {
+  title: "ESSENCIA",
+  button: {
+    player: "Jogador",
+    GM: "Mestre",
+    newGame: "Novo",
+  },
+  textBox: [
+    "Como Jogador, entre em um jogo que você foi convidado.",
+    "Como Mestre, cria suas próprias aventuras e convide jogadores.",
+  ],
+  returnMenu: {
+    player: "Selecione o Jogo",
+    GM: "Selecione o Jogo",
+  },
+};
+module.exports.playerGames = {
+  returnMenu: "Selecione o Jogo",
+  noGames: "Você ainda não participa de nenhum jogo.",
+  noCharacter: "Sem Personagem",
+  inProgress: "Em Progresso",
+  level: "Nível",
+};
+module.exports.gmGames = {
+  returnMenu: "Selecione o Jogo",
+  noGames: "Você ainda não criou nenhuma jogo.",
+};
+module.exports.newGame = {
+  returnMenu: "Escolha de Nome",
+  label: "Nome",
+  button: "Criar",
+  takenName: "Já existe um jogo com esse nome.",
+};
+
+module.exports.gmPanel = {
+  button: {
+    management: "Gerenciamento",
+    encounter: "Encontro",
+    rules: "Regras Mestre",
+    tests: "Testes",
+    tools: "Ferramentas",
+  },
+};
+module.exports.management = {
+  returnMenu: "Gerenciamento",
+  avatar: "foto",
+  button: {
+    manage: "Gerenciar",
+    add: "Adicionar",
+  },
+  label: "Notas Gerais",
+  textArea: "Notas da campanha...",
+};
+module.exports.newPlayer = {
+  returnMenu: "Adicionar Jogador",
+  label: "Nome",
+  button: "Adicionar",
+  notFound: "Usuário não encontrado.",
+  alreadyIn: "Usuário já está no jogo.",
+};
+
 module.exports.raceSelection = {
   title: "Escolha a Raça",
   race: {
--- src/utils/content.js
+++ src/utils/content.js
@@ -1,8 +1,4 @@
 const defaultLanguage = "portuguese";
 const selectedLanguage = localStorage.getItem("language") || defaultLanguage;
 
-const pages = ["components", "charCreation", "dashboard", "gmPanel", "login"];
-
-pages.forEach((page) => {
-  module.exports = require(`./Languages/${selectedLanguage}/${page}`);
-});
+module.exports = require(`./Languages/${selectedLanguage}`);
